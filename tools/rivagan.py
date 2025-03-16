44949ba794cf3bf91d78ca3a074350185c708346
pira99 <piraveen@live.nl>
2025-03-16 18:35:03 +0100

    Fixed it, I was stoopid

--- tools/rivagan.py
+++ tools/rivagan.py
@@ -6,7 +6,7 @@ class Rivagan(Tool):
     def __init__(self):
         super().__init__("rivagan")
 
-    def embed_watermark(self, data, cover_file, stego_file, method="dwtDct", watermark_type="bytes"):
+    def embed_data(self, data, cover_file, stego_file, method="dwtDct", watermark_type="bytes"):
         """
         Embeds data into a cover file to create a stego file.
 
@@ -24,7 +24,7 @@ class Rivagan(Tool):
             cover_file,
         ], check=True)
 
-    def extract_watermark(self, stego_file, output_file, method="dwtDct", watermark_type="bytes"):
+    def extract_data(self, stego_file, output_file, method="dwtDct", watermark_type="bytes", length="216"):
         """
         Extracts an invisible watermark from an image or video file.
 
@@ -32,18 +32,26 @@ class Rivagan(Tool):
         :param output_file: The output file to save the extracted watermark.
         :return: The extracted watermark data.
         """
-        subprocess.run([
-            "tools/bin/invisible-watermark/invisible-watermark", "-v",
-            "-a" , "decode",
-            "-t", watermark_type,
-            "-m", method,
-            "-o", output_file,
-            stego_file
-        ], check=True)
-
         try:
+            subprocess.run([
+                "tools/bin/invisible-watermark/invisible-watermark", "-v",
+                "-a" , "decode",
+                "-t", watermark_type,
+                "-m", method,
+                "-l", length,
+                "-o", output_file,
+                stego_file
+            ], check=True)
+
             with open(output_file, "rb") as f:
                 return f.read()
+        except subprocess.CalledProcessError as e:
+            print(f"Error during extraction execution: {e}")
+            return None
         except FileNotFoundError:
+            print(f"Output file not found: {output_file}")
             with open(output_file, "wb") as f:
                 f.write(b"-")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+            raise
